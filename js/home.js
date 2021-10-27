675fb0bd53de41d57cb701c3151231588209c496
Jose Bernal <jbernal.web.dev@gmail.com>
2021-10-27 16:00:19 -0700

    Only display posts tagged as "store".

--- js/home.js
+++ js/home.js
@@ -15,10 +15,27 @@ $(function(){
 	if(document.getElementById("promoted-posts")){
 		globalScripts(["//"+OCDLA._get('domain')+"/sites/default/modules/wordpress/posts.js"],function(wpPosts){
 			var endpoint = "//"+OCDLA._get('businessDomain')+"/wp-json/wp/v2/posts";
-
-				wpPosts.getPosts(endpoint).then( function(posts){
+			var tagEndpoint = "//"+OCDLA._get('businessDomain')+"/wp-json/wp/v2/tags";
+			
+			
+			function getTagId(name) {
+				return wpPosts.getTags(tagEndpoint).then(function(tags) {
+					console.log(tags);
+					for(let i = 0; i< tags.length; i++) {
+						if(tags[i].name == name) return tags[i].id;
+					}
+					return null;
+				});
+			}
+			
+			getTagId("store").then(function(tagId) {
+				console.log("TagId is: "+tagId);
+				wpPosts.getPosts(endpoint, tagId).then( function(posts){
 						document.getElementById("promoted-posts").innerHTML = posts.join("\n");
 				});
+			});
+				
+				
 		});
 	}
 
